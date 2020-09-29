defe481de13de70413f38a39890930ef32a3d79c
we684123 <we684123@gmail.com>
2020-09-29 17:53:19 +0800

    ✅ 通過了新的 unit test

--- unit_test/unit_test_match.ts
+++ unit_test/unit_test_match.ts
@@ -213,3 +213,120 @@ i++
 console.log(`\n${i}`);
 console.log(match(amis4, source) == true)
 // -----------------------------------------
+
+
+// ==================================================
+var source2 = {
+  "update_id": 498276656,
+  "message": {
+    "message_id": 367,
+    "from": {
+      "id": 207014603,
+      "is_bot": false,
+      "first_name": "永格天",
+      "last_name": "(則天)",
+      "username": "we684123",
+      "language_code": "zh-hant"
+    },
+    "chat": {
+      "id": -1001097080770,
+      "title": "逆流(超級)",
+      "type": "supergroup"
+    },
+    "date": 1601312056,
+    "forward_from": {
+      "id": 207014603,
+      "is_bot": false,
+      "first_name": "永格天",
+      "last_name": "(則天)",
+      "username": "we684123",
+      "language_code": "zh-hant"
+    },
+    "forward_date": 1601311963,
+    "photo": [{
+      "file_id": "AgACAgUAAx0CQWQfwgACAW9fchU4wLSwfnDwEZR4I9zJxFUlPAACMqsxGzFokFfU-3M3tdGeQ_K6Zmt0AAMBAAMCAANtAAPtfwcAARsE",
+      "file_unique_id": "AQAD8rpma3QAA-1_BwAB",
+      "file_size": 18422,
+      "width": 320,
+      "height": 180
+    }, {
+      "file_id": "AgACAgUAAx0CQWQfwgACAW9fchU4wLSwfnDwEZR4I9zJxFUlPAACMqsxGzFokFfU-3M3tdGeQ_K6Zmt0AAMBAAMCAAN4AAPufwcAARsE",
+      "file_unique_id": "AQAD8rpma3QAA-5_BwAB",
+      "file_size": 70401,
+      "width": 800,
+      "height": 450
+    }, {
+      "file_id": "AgACAgUAAx0CQWQfwgACAW9fchU4wLSwfnDwEZR4I9zJxFUlPAACMqsxGzFokFfU-3M3tdGeQ_K6Zmt0AAMBAAMCAAN5AAPvfwcAARsE",
+      "file_unique_id": "AQAD8rpma3QAA-9_BwAB",
+      "file_size": 132519,
+      "width": 1280,
+      "height": 720
+    }],
+    "caption": "不處理k"
+  }
+}
+// -----------------------------------------
+let amis5 = {
+  "and": [{
+    'targer': ["message", "forward_from"],
+    'value': '',
+    'only_exist': true,
+    'use_re': false
+  },],
+  "or": [{
+    'targer': ["message", "chat", "id"],
+    'value': '207014603',
+    'only_exist': false,
+    'use_re': false
+  }, {
+    'targer': ["message", "chat", "id"],
+    'value': '-1001097080770',
+    'only_exist': false,
+    'use_re': false
+  },],
+  "not": {
+    "and": [{
+      'targer': ["message", "caption"],
+      'value': '不處理',
+      'only_exist': false,
+      'use_re': false
+    }],
+    "or": []
+  }
+}
+i++
+console.log(`\n${i}`);
+console.log(match(amis5, source2) == true)
+// -----------------------------------------
+amis5 = {
+  "and": [{
+    'targer': ["message", "forward_from"],
+    'value': '',
+    'only_exist': true,
+    'use_re': false
+  },],
+  "or": [{
+    'targer': ["message", "chat", "id"],
+    'value': '207014603',
+    'only_exist': false,
+    'use_re': false
+  }, {
+    'targer': ["message", "chat", "id"],
+    'value': '-1001097080770',
+    'only_exist': false,
+    'use_re': false
+  },],
+  "not": {
+    "and": [{
+      'targer': ["message", "caption"],
+      'value': '不處理k',
+      'only_exist': false,
+      'use_re': false
+    }],
+    "or": []
+  }
+}
+i++
+console.log(`\n${i}`);
+console.log(match(amis5, source2) == false)
+// -----------------------------------------
