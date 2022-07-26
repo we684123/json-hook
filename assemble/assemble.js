98008c7277bf6268ab3f1b22d6c86fa0bf1d3c98
we684123 <we684123@gmail.com>
2022-07-26 23:46:39 +0800

    ✨ assemble.js 完成

--- assemble/assemble.js
+++ assemble/assemble.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 var fs = require("fs");
 const path = require("path");
+
+const prettier = require("prettier");
 const commander = require("commander");
 
 const program = new commander.Command();
@@ -43,33 +45,117 @@ program
   )
   .parse(process.argv);
 
-console.log(`optsKeys: ${Object.keys(program.opts())}`);
-console.log(`optsValues: ${Object.values(program.opts())}`);
+// console.log(`optsKeys: ${Object.keys(program.opts())}`);
+// console.log(`optsValues: ${Object.values(program.opts())}`);
 
-function list_tree_to_do(dir, hide_folder_name, func, arg) {
-  var list = [];
+function list_tree_to_do(dir, new_opts, mode, txts) {
   var arr = fs.readdirSync(dir);
   arr.forEach(function(item) {
     var fullpath = path.join(dir, item);
     var stats = fs.statSync(fullpath);
     if (stats.isDirectory()) {
       // 不是"隱藏內容"的資料夾名稱就遞迴
-      if (fullpath.split(path.sep).pop() != hide_folder_name) {
-        list_tree_to_do(fullpath);
+      if (fullpath.split(path.sep).pop() != new_opts.hide_folder_name) {
+        list_tree_to_do(fullpath, new_opts, mode, txts);
       }
     } else {
       // 是檔案就處理
-      list.push(fullpath);
-      var data_str = fs.readFileSync(fullpath).toString();
-      func.call(arg);
+      const data_str = fs.readFileSync(fullpath, "utf-8").toString();
+      if (mode == "functions") {
+        // 處理 functions
+        txts.push(data_str); // 寫入完整的 function 函式
+      } else if (mode == "plugins") {
+        // 處理 plugins
+        const re_plugins = new RegExp("^function ([^{}]+)", "g");
+        txts.push(data_str); // 寫入完整的 plugin 函式
+        txts.push(re_plugins.exec(data_str)[1]); // 再寫入 plugin 函式的名稱來達成自動載入
+      }
     }
   });
-  return list;
+  return txts;
 }
 
-function assemble_plugins(arg) {}
+function assemble(new_opts) {
+  // 讀取被組合的 main 程式
+  const main_code = fs.readFileSync(new_opts.input_file_path).toString();
+  var new_main_code = "";
+  var changed = false;
 
-function assemble() {
+  // 組合 functions
+  if (main_code.includes(new_opts.alf)) {
+    var txts_for_functions = [];
+    txts_for_functions = list_tree_to_do(
+      new_opts.functions_folder,
+      new_opts,
+      "functions",
+      txts_for_functions
+    );
+
+    let ass = txts_for_functions.join("\n");
+    new_main_code = main_code.replace(new_opts.alf, ass);
+    changed = true;
+  } else {
+    console.log(
+      `input file content have not ${new_opts.alf}, so i can not replace.`
+    );
+  }
+
+  // 組合 plugins
+  if (main_code.includes(new_opts.alp)) {
+    var txts_for_plugins = [];
+    txts_for_plugins = list_tree_to_do(
+      new_opts.plugins_folder,
+      new_opts,
+      "plugins",
+      txts_for_plugins
+    );
+
+    let ass = txts_for_plugins.join("\n");
+    new_main_code = main_code.replace(new_opts.alp, ass);
+    changed = true;
+  } else {
+    console.log(
+      "\x1B[33m%s\x1B[0m",
+      `input file content have not ${new_opts.alp}, so i can not replace.`
+    );
+  }
+
+  // 如果有成功組合就，註解掉 loadNodejsPlugin、loadGoogleAppsScriptPlugin
+  // 然後輸出檔案
+  if (changed) {
+    // 處理註解
+    const annotation = "// ↓ annotation by assemble.js";
+    const re_loadNodejsPlugin = RegExp(
+      "(^[^.\n]+.loadNodejsPlugin[^\n]+)",
+      "gm"
+    );
+    const re_loadGoogleAppsScriptPlugin = RegExp(
+      "(^[^.\n]+.loadGoogleAppsScriptPlugin[^\n]+)",
+      "gm"
+    );
+    new_main_code = new_main_code.replace(
+      re_loadNodejsPlugin,
+      `${annotation}\n// $1`
+    );
+    new_main_code = new_main_code.replace(
+      re_loadGoogleAppsScriptPlugin,
+      `${annotation}\n// $1`
+    );
+
+    // 格式化 code
+    new_main_code = prettier.format(new_main_code, {
+      semi: false,
+      parser: "babel"
+    });
+
+    fs.writeFile(new_opts.output_file_path, new_main_code, function(err) {
+      if (err) console.log(err);
+      else console.log("\x1B[36m%s\x1B[0m", "assemble done.");
+    });
+  }
+}
+
+function assemble_CLI() {
   const opts = program.opts();
 
   // 用這種方式處理輸入，配合 Hydrogen 開發較順手
@@ -94,14 +180,25 @@ function assemble() {
     functions_folder
   );
 
-  // 開始組合
-  assemble_plugins(
-    input_plugins_folder_path,
-    hide_folder_name,
-    list_tree_to_do
-  );
-}
-assemble();
+  const new_opts = {
+    input: input,
+    output: output,
+    plugins_folder: plugins_folder,
+    functions_folder: functions_folder,
+    alp: alp,
+    alf: alf,
+    hide_folder_name: hide_folder_name,
+    input_file_path: input_file_path,
+    output_file_path: output_file_path,
+    input_plugins_folder_path: input_plugins_folder_path,
+    input_functions_folder_path: input_functions_folder_path
+  };
+  console.log("\x1B[36m%s\x1B[0m", "【info】new_opts：");
+  console.log(new_opts);
 
+  // 開始組合
+  assemble(new_opts);
+}
+assemble_CLI();
 
 //
