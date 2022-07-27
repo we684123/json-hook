a97709ed5b816791ef46c01366fcfe3bb828591f
we684123 <we684123@gmail.com>
2022-07-27 16:37:52 +0800

    🚨 hook.ts、match.ts 在 .eslintrc.js 的監督下改了一堆

--- hook.ts
+++ hook.ts
@@ -36,9 +36,9 @@ export class JsonHook {
    * @returns {void}
    */
   public listHook(): void {
-    let hookList = this.hooks
+    const hookList = this.hooks
     console.log(hookList)
-    for (let [key, value] of hookList) {
+    for (const [key, value] of hookList) {
       console.log(key);
       console.log(value);
     }
@@ -79,7 +79,7 @@ export class JsonHook {
     incoming?: any,
     strict_equality?: boolean
   ): void {
-    for (let [hook_aims, hook_function] of this.hooks) {
+    for (const [hook_aims, hook_function] of this.hooks) {
       if (match(hook_aims, source, strict_equality)) { // 條件符合，執行!
         hook_function.call(null, incoming)
       }
@@ -99,11 +99,11 @@ export class JsonHook {
     hook_name: string
   ): void {
     hook // ㄜ... 對! 沒有用~ 只是不想看到 ts 一直提醒我沒有用 (๑-﹏-๑)
-    for (var key in _this) {
+    for (const key in _this) {
       if (typeof _this[key] == "function") {
-        let regex = RegExp(String(this.plugin_re_str), 'g')
-        if (!!String(key).match(regex)) {
-          let cmd = `_this.${key}(${hook_name})`
+        const regex = RegExp(String(this.plugin_re_str), 'g')
+        if (String(key).match(regex)) {
+          const cmd = `_this.${key}(${hook_name})`
           eval(`var ${hook_name} = hook`)
           eval(cmd)
         }
@@ -122,42 +122,38 @@ export class JsonHook {
     hook_name: string
   ): void {
     hook // ㄜ... 對! 沒有用~ 只是不想看到 ts 一直提醒我沒有用 (๑-﹏-๑)
-    // @ts-ignore
     const path = require('path');
-    // @ts-ignore
     const fs = require('fs');
 
     const plugins_folder = this.plugins_folder
-    // @ts-ignore
     const directoryPath = path.join(process.cwd(), plugins_folder);
 
     const file_name_re = RegExp('.+\.(ts|js|gs)$', 'g')
     const get_plugin_function_name_re = RegExp('^function\ ([^{}]+)', 'g')
-    let ignore_load_plugin_error = this.ignore_load_plugin_error
-    let load_plugin_log = this.load_plugin_log
+    const ignore_load_plugin_error = this.ignore_load_plugin_error
+    const load_plugin_log = this.load_plugin_log
 
 
-    var list: any[] = [];
+    const list: any[] = [];
     function listFile(dir: string) {
       // 感謝 luffy5459
       // https://blog.csdn.net/feinifi/article/details/106109495
-      var arr = fs.readdirSync(dir);
+      const arr = fs.readdirSync(dir);
       arr.forEach(function(item: any) {
-        var fullpath = path.join(dir, item);
-        var stats = fs.statSync(fullpath);
+        const fullpath = path.join(dir, item);
+        const stats = fs.statSync(fullpath);
         if (stats.isDirectory()) {
           listFile(fullpath);
         } else {
           list.push(fullpath);
 
-          if (!!String(fullpath).match(file_name_re)) {
+          if (String(fullpath).match(file_name_re)) {
             if (load_plugin_log) {
               console.log(fullpath);
             }
-            // @ts-ignore
-            var data_str = fs.readFileSync(fullpath).toString();
-            var i = data_str.match(get_plugin_function_name_re);
-            var j = i[0].replace('function ', '');
+            const data_str = fs.readFileSync(fullpath).toString();
+            const i = data_str.match(get_plugin_function_name_re);
+            const j = i[0].replace('function ', '');
             // 載入 plugin function 內容
             try {
               eval(data_str + '\n' + j.replace('hook', hook_name));
@@ -176,18 +172,5 @@ export class JsonHook {
       return list;
     }
     listFile(directoryPath);
-
-    // // @ts-ignore
-    // var files = fs.readdirSync(directoryPath);
-    // files.forEach(function (file: any) {
-    //   if (!!String(file).match(file_name_re)) {
-    //     // @ts-ignore
-    //     var data_str = fs.readFileSync("./" + plugins_folder + "/" + file).toString();
-    //     var i = data_str.match(get_plugin_function_name_re);
-    //     var j = i[0].replace('function ', '');
-    //     // 載入 plugin function 內容
-    //     eval(data_str + '\n' + j.replace('hook', hook_name));
-    //   }
-    // })
   }
 }
--- match.ts
+++ match.ts
@@ -5,9 +5,9 @@
  */
 export function check_parameter(par: any, from: string): void {
   //這裡確認 targer value... 都在
-  for (var i = 0; i < par.length; i++) {
+  for (let i = 0; i < par.length; i++) {
     // i = 0
-    let par_i = par[i]
+    const par_i = par[i]
     if (par_i['targer'] == undefined) {
       throw `'${from}' Array ${i} targer is miss`
     }
@@ -52,13 +52,13 @@ export function match_par(
   // console.log(`source = ${source}`);
 
   // 回傳比對結果的 boolean[]
-  let result: boolean[] = []
+  const result: boolean[] = []
   // var iterator = aims_par[1]
   for (const iterator of aims_par) {
 
     // console.log(`iterator = ${JSON.stringify(iterator)}`);
-    let yn = source
-    var rt = match_iterator(iterator, yn, strict_equality)
+    const yn = source
+    const rt = match_iterator(iterator, yn, strict_equality)
     result.push(rt)
   }
   // console.log("外層");
@@ -114,7 +114,7 @@ export function match_iterator(
         if (iterator['use_re']) { // 用正則表達式比對
           // console.log(`iterator['use_re'] = ${String(iterator['use_re'])}`);
           // console.log(`iterator['value'] = ${String(iterator['value'])}`);
-          let regex = RegExp(String(iterator['value']), 'g')
+          const regex = RegExp(String(iterator['value']), 'g')
           return !!String(yn).match(regex)
         } else {  // 不用正則表達式比對
           // 看要不要用 "===" 進行比對
@@ -151,19 +151,19 @@ export function match(
   // var strict_equality = false
 
   // 先基礎定義下面4個，順便檢查
-  var and = aims['and']
-  var or = aims['or']
-  var not_and = aims ?.not ?.and
-  var not_or = aims ?.not ?.or
+  const and = aims['and']
+  const or = aims['or']
+  const not_and = aims ?.not ?.and
+  const not_or = aims ?.not ?.or
   if (and === undefined && or === undefined) {
     throw "'and' and 'or' at least give one.";
   }
 
   // 以下4個的 boolean[] 用來儲存比對結果
-  var and_list: boolean[] = []
-  var or_list: boolean[] = []
-  var not_and_list: boolean[] = []
-  var not_or_list: boolean[] = []
+  let and_list: boolean[] = []
+  let or_list: boolean[] = []
+  let not_and_list: boolean[] = []
+  let not_or_list: boolean[] = []
 
   // 以下比對並儲存比對結果
   if (and === undefined) {
@@ -201,19 +201,19 @@ export function match(
   // 例如用 and 的就必須每項都符合才能給 true，所以用 Array.every 檢查
   // 而用 or 的只要一項符合就可以給 true，所以用 Array.some 檢查
   // not_and_list 跟 not_or_list 前面已經用 map 反轉過了
-  var and_list_result = and_list.every(function(item) {
+  const and_list_result = and_list.every(function(item) {
     return item === true
   });
-  var or_list_result = or_list.some(function(item) {
+  const or_list_result = or_list.some(function(item) {
     return item === true
   });
-  var not_and_list_result = not_and_list.every(function(item) {
+  const not_and_list_result = not_and_list.every(function(item) {
     return item === true
   });
-  var not_or_list_result = not_or_list.some(function(item) {
+  const not_or_list_result = not_or_list.some(function(item) {
     return item === true
   });
-  var last_result = [
+  const last_result = [
     and_list_result,
     or_list_result,
     not_and_list_result,
